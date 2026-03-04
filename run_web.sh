c8842ea766d7fa371a8fa11e96264a4792330f38
jim <904165900@qq.com>
2026-03-04 11:05:44 +0800

    Update run_web.sh

--- run_web.sh
+++ run_web.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Web 服务模式（推荐，提供 API 与 WebUI）
-docker-compose -f ./docker/docker-compose.yml up -d server  
+# Web 服务模式（推荐，提供 API 与 WebUI），加上--build这个每次执行就会重启
+docker-compose -f ./docker/docker-compose.yml up -d --build server  
